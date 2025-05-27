3d785ebdedb3442bdb0332a8d8d04d3fe443b4ee
João Pedro Soeiro Lemos <joaopedro@tec-adm.com.br>
2025-05-27 16:17:59 -0300

    feat: enhance authentication flow with session checks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
-const DashboardPage = () => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Bem-vindo, {session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      <SignOutButton />
     </div>
   );
 };
