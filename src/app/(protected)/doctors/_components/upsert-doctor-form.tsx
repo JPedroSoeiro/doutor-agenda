02098ce3da72a08ffd8c92c24fbc6e138c55ec13
João Pedro Soeiro Lemos <joaopedro@tec-adm.com.br>
2025-05-28 16:24:30 -0300

    feat: add doctor name

---
+++ src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
@@ -0,0 +1,84 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+  speciality: z
+    .string()
+    .trim()
+    .min(1, { message: "Especialidade é obrigatória" }),
+  appointmentPrice: z
+    .number()
+    .min(1, { message: "Preço da consulta é obrigatório" }),
+  availableFromWeekDay: z.number(),
+  availableToWeekDay: z.number(),
+  availableFromTime: z
+    .string()
+    .min(1, { message: "Horário de início é obrigatório" }),
+  availableToTime: z
+    .string()
+    .min(1, { message: "Horário de término é obrigatório" }),
+});
+
+const UpsertDoctorForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      speciality: "",
+      appointmentPrice: 0,
+      availableFromWeekDay: 0,
+      availableToWeekDay: 0,
+      availableFromTime: "",
+      availableToTime: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Adicionar Médico</DialogTitle>
+        <DialogDescription>
+          Adicione um novo médico à sua clínica
+        </DialogDescription>
+      </DialogHeader>
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </FormProvider>
+    </DialogContent>
+  );
+};
+
+export default UpsertDoctorForm;
