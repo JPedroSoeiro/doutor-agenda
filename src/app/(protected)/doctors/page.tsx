3107edfa9cb7ff193c0c093aa32fc4a42fc7325b
João Pedro Soeiro Lemos <joaopedro@tec-adm.com.br>
2025-05-28 13:46:52 -0300

    feat: create page container components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,32 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
